1bfab6e4c6a8a4d912c3ca61cd70ace8e7ef7e7a
jamelreid <jnoelreid@gmail.com>
2021-11-30 14:49:40 -0500

    Set jest.retryTimes wih the E2E_RETRY_TIME env

--- plugins/woocommerce/tests/e2e/config/jest.setup.js
+++ plugins/woocommerce/tests/e2e/config/jest.setup.js
@@ -8,7 +8,7 @@ import {
 const config = require( 'config' );
 const { HTTPClientFactory } = require( '@woocommerce/api' );
 const { addConsoleSuppression, updateReadyPageStatus } = require( '@woocommerce/e2e-environment' );
-const { DEFAULT_TIMEOUT_OVERRIDE } = process.env;
+const { DEFAULT_TIMEOUT_OVERRIDE, E2E_RETRY_TIMES } = process.env;
 
 // @todo: remove this once https://github.com/woocommerce/woocommerce-admin/issues/6992 has been addressed
 addConsoleSuppression( 'woocommerce_shared_settings', false );
@@ -40,6 +40,8 @@ async function trashExistingPosts() {
 // each other's side-effects.
 beforeAll(async () => {
 
+	const retryTimes = E2E_RETRY_TIMES ? E2E_RETRY_TIMES : 3;
+
 	if ( DEFAULT_TIMEOUT_OVERRIDE ) {
 		page.setDefaultNavigationTimeout( DEFAULT_TIMEOUT_OVERRIDE );
 		page.setDefaultTimeout( DEFAULT_TIMEOUT_OVERRIDE );
@@ -59,6 +61,9 @@ beforeAll(async () => {
 	await page.goto(WP_ADMIN_LOGIN);
 	await clearLocalStorage();
 	await setBrowserViewport('large');
+
+	// Set how many times to retry failed tests.
+	jest.retryTimes( retryTimes );
 });
 
 // Clear browser cookies and cache using DevTools.
